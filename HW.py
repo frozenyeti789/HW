e0de2551c3d772d75fb3c2376a1a453e3efe8311
frozenyeti789 <frozenyeti789@gmail.com>
2016-07-28 16:11:44 +0600

    Add files via upload

---
+++ HW.py
@@ -0,0 +1,24 @@
+word = "dnfkjejevfvkvv/hkfhukf/"
+print(word.find("/"))
+print(word.split("/"))
+
+word2 = "blalalsdjsfff.,fhef.,hfgeufy"
+print(word2.find(".,"))
+print(word2.split(".,"))
+
+def prog():
+    a = input(r'Enter whatever:')
+    print(a)
+
+prog()
+
+
+def prog2():
+    a = input(r'Enter stuff:')
+    print(a.find(""))
+
+prog2()
+
+
+a = input("Enter a word:  ")
+print(a.count(""))
